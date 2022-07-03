1ea7b331666344716ce69a8de99be42867d047e0
JohnElvisMedeirosCosta <jooohn.elvis@hotmail.com>
2022-07-03 19:03:21 -0300

    Teste

---
+++ Desafio 2.js
@@ -0,0 +1,109 @@
+// Crie uma função que receba dois argumentos e retorne a soma dos mesmos.
+function soma(x, y) {
+    return x + y;
+}
+
+// Declare uma variável que receba a invocação da função criada acima, passando dois números quaisquer por argumento, e somando `5` ao resultado retornado da função.
+var minhasoma = soma(1, 4) + 5;
+
+// Qual o valor atualizado dessa variável?
+10
+
+// Declare uma nova variável, sem valor.
+var vazia;
+
+/*
+Crie uma função que adicione um valor à variável criada acima, e retorne a string:
+    O valor da variável agora é VALOR.
+Onde VALOR é o novo valor da variável.
+*/
+function adicionar() {
+    vazia = 25;
+    return 'O valor da variável agora é ' + vazia;
+}
+
+// Invoque a função criada acima.
+adicionar();
+
+// Qual o retorno da função? (Use comentários de bloco).
+//O valor da variável agora é 1
+
+/*
+Crie uma função com as seguintes características:
+1. A função deve receber 3 argumentos;
+2. Se qualquer um dos três argumentos não estiverem preenchidos, a função deve retornar a string:
+    Preencha todos os valores corretamente!
+3. O retorno da função deve ser a multiplicação dos 3 argumentos, somando `2` ao resultado da multiplicação.
+*/
+function multiplos(x, y, z) {
+    if (z === undefined || y === undefined || z === undefined) {
+        return 'Preencha todos os valores corretamente!';
+    } else
+        return (x * y * z) + 2;
+}
+
+function multiplos(x, y, z) {
+    if (x === undefined) {
+        return 'Preencha todos os valores corretamente!';
+    } else if (y === undefined) {
+        return 'Preencha todos os valores corretamente!';
+    } else if (z === undefined) {
+        return 'Preencha todos os valores corretamente!';
+    } else
+        return (x * y * z) + 2;
+}
+
+// Invoque a função criada acima, passando só dois números como argumento.
+multiplos(1,2);
+
+// Qual o resultado da invocação acima? (Use comentários para mostrar o valor retornado).
+//'Preencha todos os valores corretamente'
+
+// Agora invoque novamente a função criada acima, mas passando todos os três argumentos necessários.
+multiplos(1,2,3);
+
+// Qual o resultado da invocação acima? (Use comentários para mostrar o valor retornado).
+
+
+/*
+Crie uma função com as seguintes características:
+1. A função deve receber 3 argumentos.
+2. Se somente um argumento for passado, retorne o valor do argumento.
+3. Se dois argumentos forem passados, retorne a soma dos dois argumentos.
+4. Se todos os argumentos forem passados, retorne a soma do primeiro com o segundo, e o resultado, dividido pelo terceiro.
+5. Se nenhum argumento for passado, retorne o valor booleano `false`.
+6. E ainda, se nenhuma das condições acima forem atendidas, retorne `null`.
+*/
+function division(x, y, z) {
+    if (x == undefined) {
+        return false;
+    } else if (y == undefined) {
+        return x;
+    } else if (z == undefined) {
+        return x + y;
+    } else if (z != undefined) {
+        return (x + y) / z;
+    } else {
+        return null;
+    }
+}
+
+function division(x, y, z) {
+    if (x !== undefined & y == undefined || z == undefined) {
+        return x;
+    } else if ( x !== undefined || y !== undefined || z == undefined) {
+        return x + y;
+    } else if ( x !== undefined || y !== undefined || z !== undefined){
+        return ( x + y ) / z;
+    } else if (x == undefined || y == undefined || z == undefined) {
+        return false;
+    } else {
+        return null;
+    }
+}
+
+// Invoque a função acima utilizando todas as possibilidades (com nenhum argumento, com um, com dois e com três.) Coloque um comentário de linha ao lado da função com o resultado de cada invocação.
+division() // false
+division(1) // 1
+division(1,2) // 3
+division(1,2,3) //1
---
+++ desafio 3.js
@@ -0,0 +1,200 @@
+// Declarar uma variável qualquer, que receba um objeto vazio.
+var objeto = {};
+
+/*
+Declarar uma variável `pessoa`, que receba suas informações pessoais.
+As propriedades e tipos de valores para cada propriedade desse objeto devem ser:
+- `nome` - String
+- `sobrenome` - String
+- `sexo` - String
+- `idade` - Number
+- `altura` - Number
+- `peso` - Number
+- `andando` - Boolean - recebe "falso" por padrão
+- `caminhouQuantosMetros` - Number - recebe "zero" por padrão
+*/
+var pessoa = {
+    nome: 'John',
+    sobrenome: 'Costa',
+    sexo: 'Masculino',
+    idade: 26,
+    altura: 1.86,
+    peso: 90.0,
+    andando: false,
+    caminhouQuantosMetros: 0
+}
+
+/*
+Adicione um método ao objeto `pessoa` chamado `fazerAniversario`. O método deve
+alterar o valor da propriedade `idade` dessa pessoa, somando `1` a cada vez que
+for chamado.
+*/
+pessoa.fazeraniversario = function () {
+    pessoa.idade++;
+}
+/*
+Adicione um método ao objeto `pessoa` chamado `andar`, que terá as seguintes
+características:
+- Esse método deve receber por parâmetro um valor que representará a quantidade
+de metros caminhados;
+- Ele deve alterar o valor da propriedade `caminhouQuantosMetros`, somando ao
+valor dessa propriedade a quantidade passada por parâmetro;
+- Ele deverá modificar o valor da propriedade `andando` para o valor
+booleano que representa "verdadeiro";
+*/
+pessoa.andar = function (x) {
+    pessoa.caminhouQuantosMetros += x;
+    if (pessoa.caminhouQuantosMetros != 0) {
+        pessoa.andando = true
+    }
+}
+
+/*
+Adicione um método ao objeto `pessoa` chamado `parar`, que irá modificar o valor
+da propriedade `andando` para o valor booleano que representa "falso".
+*/
+pessoa.parar = function () {
+    pessoa.andando = false
+}
+
+/*
+Crie um método chamado `nomeCompleto`, que retorne a frase:
+- "Olá! Meu nome é [NOME] [SOBRENOME]!"
+*/
+pessoa.nomeCompleto = function () {
+    "Olá! Meu nome é " + pessoa.nome + " " + pessoa.sobrenome + "!";
+}
+
+/*
+Crie um método chamado `mostrarIdade`, que retorne a frase:
+- "Olá, eu tenho [IDADE] anos!"
+*/
+pessoa.mostrarIdade = function () {
+    "Olá, eu tenho " + pessoa.idade + " anos!"
+}
+
+/*
+Crie um método chamado `mostrarPeso`, que retorne a frase:
+- "Eu peso [PESO]Kg."
+*/
+pessoa.mostrarPeso = function () {
+    'Eu peso ' + pessoa.peso + 'Kg.'
+}
+
+/*
+Crie um método chamado `mostrarAltura` que retorne a frase:
+- "Minha altura é [ALTURA]m."
+*/
+pessoa.mostrarAltura = function () {
+    'Minha altura é ' + pessoa.altura + 'm.'
+}
+
+
+/*
+Agora vamos brincar um pouco com o objeto criado:
+Qual o nome completo da pessoa? (Use a instrução para responder e comentários
+inline ao lado da instrução para mostrar qual foi a resposta retornada)
+*/
+pessoa.nomeCompleto(); // 'Olá! Meu nome é John Costa!'
+
+/*
+Qual a idade da pessoa? (Use a instrução para responder e comentários
+inline ao lado da instrução para mostrar qual foi a resposta retornada)
+*/
+pessoa.idade(); // 26
+
+/*
+Qual o peso da pessoa? (Use a instrução para responder e comentários
+inline ao lado da instrução para mostrar qual foi a resposta retornada)
+*/
+pessoa.peso(); // 90
+
+/*
+Qual a altura da pessoa? (Use a instrução para responder e comentários
+inline ao lado da instrução para mostrar qual foi a resposta retornada)
+*/
+pessoa.altura(); // 1.85
+
+/*
+Faça a `pessoa` fazer 3 aniversários.
+*/
+for ( var i = 0; i < 3; i++) {
+    pessoa.fazeraniversario();
+}
+
+/*
+Quantos anos a `pessoa` tem agora? (Use a instrução para responder e
+comentários inline ao lado da instrução para mostrar qual foi a resposta
+retornada)
+*/
+pessoa.mostrarIdade(); // 30
+
+/*
+Agora, faça a `pessoa` caminhar alguns metros, invocando o método `andar` 3x,
+com metragens diferentes passadas por parâmetro.
+*/
+for ( var i = 0; i < 3; i++) {
+    pessoa.andar(Math.floor(Math.random() * 10 + 1))
+}
+
+/*
+A pessoa ainda está andando? (Use a instrução para responder e comentários
+inline ao lado da instrução para mostrar qual foi a resposta retornada)
+*/
+pessoa.andando; // true
+
+/*
+Se a pessoa ainda está andando, faça-a parar.
+*/
+pessoa.parar();
+
+/*
+E agora: a pessoa ainda está andando? (Use uma instrução para responder e
+comentários inline ao lado da instrução para mostrar a resposta retornada)
+*/
+pessoa.andando; // false
+
+/*
+Quantos metros a pessoa andou? (Use uma instrução para responder e comentários
+inline ao lado da instrução para mostrar a resposta retornada)
+*/
+pessoa.caminhouQuantosMetros; //33
+
+/*
+Agora vamos deixar a brincadeira um pouco mais divertida! :D
+Crie um método para o objeto `pessoa` chamado `apresentacao`. Esse método deve
+retornar a string:
+- "Olá, eu sou o [NOME COMPLETO], tenho [IDADE] anos, [ALTURA], meu peso é [PESO] e, só hoje, eu já caminhei [CAMINHOU QUANTOS METROS] metros!"
+
+Só que, antes de retornar a string, você vai fazer algumas validações:
+- Se o `sexo` de `pessoa` for "Feminino", a frase acima, no início da
+apresentação, onde diz "eu sou o", deve mostrar "a" no lugar do "o";
+- Se a idade for `1`, a frase acima, na parte que fala da idade, vai mostrar a
+palavra "ano" ao invés de "anos", pois é singular;
+- Se a quantidade de metros caminhados for igual a `1`, então a palavra que
+deve conter no retorno da frase acima é "metro" no lugar de "metros".
+- Para cada validação, você irá declarar uma variável localmente (dentro do
+método), que será concatenada com a frase de retorno, mostrando a resposta
+correta, de acordo com os dados inseridos no objeto.
+*/
+pessoa.apresentacao = function () {
+    var sexo = 'o ';
+    var idade = 'anos ';
+    var distancia = 'metros ';
+    if (pessoa.sexo === 'Femenino ') {
+        sexo = 'a ';
+    }
+
+    if (pessoa.idade === 1) {
+        idade = 'ano'
+    }
+
+    if (pessoa.caminhouQuantosMetros === 1) {
+        distancia = 'metro'
+    }
+
+    return "Olá, eu sou " + sexo + pessoa.nome + " " + pessoa.sobrenome + ", tenho " + pessoa.idade + " " + idade + ", " + pessoa.altura + "m, meu peso é " + pessoa.peso + "kg e, só hoje, eu já caminhei " + pessoa.caminhouQuantosMetros + " " + distancia + "!";
+}
+
+// Agora, apresente-se ;)
+pessoa.apresentacao(); //'Olá, eu sou o John Costa, tenho 26 anos, 1.86m, meu peso é 90kg e, só hoje, eu já caminhei 1 metro!'
